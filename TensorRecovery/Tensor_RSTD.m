b11ab81f28678d4cd562e5aaa1742c23ddceae38
ShunleyZhang <372110305@qq.com>
2015-07-27 12:25:52 +0800

    Parameter Debug
    Now the code can work with images.

--- TensorRecovery/Tensor_RSTD.m
+++ TensorRecovery/Tensor_RSTD.m
@@ -1,18 +1,23 @@
-T = zeros(5,5,5);
-for i=1:5
-for j=1:5
-for k=1:5
-T(i,j,k)=max(i,j);
-end
-end
-end
-T(1,2,3) = 0;
-% T = double(imread('E:\Users\Desktop\Tensor\testImg.png'));
-alpha = [1,1,1];
+% T = zeros(5,5,5);
+% for i=1:5
+% for j=1:5
+% for k=1:5
+% T(i,j,k)=max(i,j);
+% end
+% end
+% end
+% for i= 1:5
+%     T(i,6-i,6-i) = 0;
+%     T(i,6-i,i) = 0;
+% end
+
+  T = double(imread('F:\cadcg410\Desktop\wall.png'));
+alpha = [1,1,1e-3];
+alpha = alpha / sum(alpha);
 beta = alpha;
-gamma = alpha/10;
-lambda =  alpha;
-eta = 0.1;
+gamma = alpha*100;
+lambda =  alpha*3000;               %lambda up rank down   1000,10000 问题在error的大小和1范数被限制了
+eta = 1;
 %T -    n mode tensor of observations/data (required input)
 %alpha
 %beta
@@ -40,8 +45,8 @@ t = 1;
 
 iter = 0;
 converged = false;
-stopCriterion = 3;
-maxIter = 3;
+stopCriterion = 1;
+maxIter = 100;
 tol = 1e-14;
 Tnorm = norm(Unfold(T,size(T),1), 'fro');
 %% main loop
@@ -54,26 +59,42 @@ while ~converged
         S_i = Unfold(S, size(S), i);
         temp = (beta(i)*S_i+gamma(i)*(A{i}-M{i}))/(beta(i)+gamma(i));
         tau = eta/(beta(i)+gamma(i));
+%         disp(temp);
+%         disp(tau);
         N_i = max(temp-tau, 0);
+        tmps = size(temp);
+        for k=1:tmps(1)
+            for j=1:tmps(2)
+                if (temp(k,j)<-tau)
+                    N_i(k,j) = min(temp(k,j)+tau,0);
+                end
+            end
+        end
+        
+ %       disp(N_i);
+        N{i} = N_i;
         %% compute M_i
         L_i = Unfold(L, size(L), i);
         tempuv = (alpha(i)*L_i+gamma(i)*(A{i}-N{i}))/(alpha(i)+gamma(i));
         [U,DIG,V] = svd(tempuv, 'econ');
-      % disp(DIG);
+        
         diagS = diag(DIG);
         tau = lambda(i)/(alpha(i)+gamma(i));
         svp = length(find(diagS > tau));
         sap = length(find(diagS > 0));
-        fprintf('%d %d\n',svp,sap);
+  %      fprintf('%d %d\n',svp,sap);
         afterD = max(diagS(1:svp)-tau, 0);
+      %  disp(DIG);
+       % disp(afterD);
         M_i = U(:,1:svp)*diag(afterD)*V(:,1:svp)';
-        %disp(M_i);
+        %disp(M_i-M{i});
         
-        disp(norm(M_i,  'fro'));
-        disp(norm(M{i},  'fro'));
+%         disp(norm(M_i,  'fro'));
+%         disp(norm(M{i},  'fro'));
         %% update
-        N{i} = N_i;
+        
         M{i} = M_i;
+       % disp(M_i-M{i});
     end
     
     %% compute L and S
@@ -102,4 +123,11 @@ while ~converged
         converged = 1 ;       
     end
 end
+% disp(L_star);
 
+subplot(1,3,1);
+imshow(uint8(T));
+subplot(1,3,2);
+imshow(uint8(L_star));
+subplot(1,3,3);
+imshow(uint8(S_star));
